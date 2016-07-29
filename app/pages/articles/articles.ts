947d65498154d00c31a7f33683d1208e3cff0e29
Aurora Nou <auroranou@gmail.com>
2016-07-29 00:14:23 -0400

    add articles service provider

--- app/pages/articles/articles.ts
+++ app/pages/articles/articles.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams, Platform } from 'ionic-angular';
+import { ArticlesService } from '../../providers/articles-service/articles-service';
 
 /*
   Generated class for the ArticlesPage page.
@@ -9,20 +10,33 @@ import { NavController, NavParams, Platform } from 'ionic-angular';
 */
 @Component({
   templateUrl: 'build/pages/articles/articles.html',
+  providers: [ArticlesService]
 })
 export class ArticlesPage {
     public tripMessage: any;
     public tripTime: any;
+    public articles: any;
 
   constructor(
     private nav: NavController,
     private navParams: NavParams,
-    public platform: Platform
+    public platform: Platform,
+    private articlesSvc: ArticlesService
   ) {
     this.navParams = navParams;
     this.platform = platform;
     this.tripMessage = navParams.get("tripMessage");
     this.tripTime = navParams.get("tripTime");
+    
+    this.getArticles();
+  }
+
+  getArticles() {
+    this.articlesSvc.getArticles(this.tripTime)
+    .then(data => {
+      // TO DO: Randomly select 5 articles before binding to this
+      this.articles = data;
+    });
   }
 
 }
